8f46d4d74fb9446a083a5645c5b92aab64709fc0
Dominic Letz <dominic@diode.io>
2025-07-02 23:55:00 +0200

    Updated upgrade script to support local dev

--- scripts/upgrade_canister.exs
+++ scripts/upgrade_canister.exs
@@ -8,12 +8,21 @@ w =
   |> DiodeClient.Base16.decode()
   |> DiodeClient.Wallet.from_privkey()
 
-factory = "dgnum-qiaaa-aaaao-qj3ta-cai"
-
 case System.argv() do
-  [destination_text, chain] ->
+  [env, destination_text, chain] ->
+    factory =
+      case env do
+        "local" ->
+          System.put_env("ICP_DOMAIN", "http://127.0.0.1:4943")
+          {id, 0} = System.cmd("dfx", ["canister", "id", "CanisterFactory"])
+          String.trim(id)
+
+        "ic" ->
+          "dgnum-qiaaa-aaaao-qj3ta-cai"
+      end
+
     destination = ICPAgent.decode_textual(destination_text)
-    {_, 0} = System.cmd("dfx", ["build", "ZoneAvailabilityCanister"])
+    {_, 0} = System.cmd("dfx", ["build", "--check", "ZoneAvailabilityCanister"])
 
     wasm =
       File.read!("./.dfx/local/canisters/ZoneAvailabilityCanister/ZoneAvailabilityCanister.wasm")
@@ -30,6 +39,7 @@ case System.argv() do
       end
 
     type = %{zone_id: :text, rpc_host: :text, rpc_path: :text, cycles_requester_id: :principal}
+
     values = %{
       zone_id: zone_id,
       rpc_host: rpc_host,
@@ -39,14 +49,22 @@ case System.argv() do
 
     args = Candid.encode_parameters([{:record, type}], [values])
 
-    [] = ICPAgent.call(factory, w, "install_code", [:principal, :blob, :blob], [
-      destination,
-      wasm,
-      args
-    ])
+    [] =
+      ICPAgent.call(factory, w, "install_code", [:principal, :blob, :blob], [
+        destination,
+        wasm,
+        args
+      ])
 
-    IO.puts("Done! Review at: https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/?id=#{destination_text}")
+    url =
+      if env == "local" do
+        "http://127.0.0.1:4943/?canisterId=u6s2n-gx777-77774-qaaba-cai&id="
+      else
+        "https://a4gq6-oaaaa-aaaab-qaa4q-cai.raw.icp0.io/?id="
+      end
+
+    IO.puts("Done! Review at: #{url}#{destination_text}")
 
   _ ->
-    IO.puts("Usage: upgrade_canister.exs <destination> (moonbeam|diode)")
+    IO.puts("Usage: upgrade_canister.exs (local|ic) <destination> (moonbeam|diode)")
 end
